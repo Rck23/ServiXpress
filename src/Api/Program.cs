7d2ad61be3907be044ac33caabbb97ce8ebe0048
rck23 <ulimxtz@gmail.com>
2023-09-15 11:05:49 -0600

    Entornos de ejecucion separados

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,5 +1,10 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using ServiXpress.Application;
+using ServiXpress.Domain;
 using ServiXpress.Infrastructure;
 using ServiXpress.Infrastructure.Context;
 
@@ -8,6 +13,7 @@ using ServiXpress.Infrastructure.Context;
 var builder = WebApplication.CreateBuilder(args);
 
 IWebHostEnvironment _env = builder.Environment;
+
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -24,10 +30,31 @@ builder.Configuration.AddJsonFile($"appsettings.{_env.EnvironmentName}.json", op
 // AGREGACION DE CONTEXT
 builder.Services.AddDbContext<ServiXpressDbContext>(options =>
 {
+   
     options.UseSqlServer(builder.Configuration.GetConnectionString("Conexion"),
         b => b.MigrationsAssembly(typeof(ServiXpressDbContext).Assembly.FullName));
     // para que aparezca en consola todos los querys que se hacen en la utilizacion del sistema.
 });
+
+//IdentityBuilder identityBuilder = builder.Services.AddIdentityCore<Usuario>();
+
+//identityBuilder = new IdentityBuilder(identityBuilder.UserType, identityBuilder.Services);
+
+//identityBuilder.AddRoles<IdentityRole>().AddDefaultTokenProviders();
+//identityBuilder.AddClaimsPrincipalFactory<UserClaimsPrincipalFactory<Usuario, IdentityRole>>();
+
+//identityBuilder.AddEntityFrameworkStores<ServiXpressDbContext>();
+//identityBuilder.AddSignInManager<SignInManager<Usuario>>();
+
+//builder.Services.TryAddSingleton<ISystemClock, SystemClock>();
+
+//builder.Services.AddCors(options =>
+//{
+//    options.AddPolicy("CorsPolicy", builder => builder.AllowAnyOrigin()
+//        .AllowAnyMethod()
+//        .AllowAnyHeader()
+//    );
+//});
 
 
 
@@ -38,16 +65,53 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+   
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
 
+app.UseCors("CorsPolicy");
+
 app.MapControllers();
+
+//// PONER DATOS AUTOMATICAMENTE EN LA BASE DE DATOS
+//using (var scope = app.Services.CreateScope())
+//{
+//    var service = scope.ServiceProvider;
+//    var loggerFactory = service.GetRequiredService<ILoggerFactory>();
+
+//    // INSTANCIAR EL CONTEXT, ROLES, USUARIOS, ETC....
+//    try
+//    {
+//        var context = service.GetRequiredService<ServiXpressDbContext>();
+//        var usuarioManager = service.GetRequiredService<UserManager<Usuario>>();
+//        var rolManager = service.GetRequiredService<RoleManager<IdentityRole>>();
+
+//        // MIGRAR 
+//        await context.Database.MigrateAsync();
+
+//        //LLAMAR A LA DATA
+//        await ServiXpressDbContextData.LoadDataAsync(context, usuarioManager, rolManager, loggerFactory);
+
+//    }
+//    catch (Exception ex)
+//    {
+//        var logger = loggerFactory.CreateLogger<Program>();
+//        logger.LogError(ex, "Error al migrar los datos");
+//    }
+
+//}
+
+
 
 app.Run();
