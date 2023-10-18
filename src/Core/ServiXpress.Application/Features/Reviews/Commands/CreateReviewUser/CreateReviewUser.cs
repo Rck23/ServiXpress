103fee36547d3e8702354185b3b6f248a2b3b17e
Isaac Gallegos <jesusisaac360@gmail.com>
2023-10-18 00:46:53 -0600

    Revert changes from merge master commit

--- src/Core/ServiXpress.Application/Features/Reviews/Commands/CreateReviewUser/CreateReviewUser.cs
+++ src/Core/ServiXpress.Application/Features/Reviews/Commands/CreateReviewUser/CreateReviewUser.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using MediatR;
 using ServiXpress.Application.Features.Reviews.ViewModels;
 
@@ -8,10 +9,18 @@ namespace ServiXpress.Application.Features.Reviews.Commands.CreateReviewService
     {
 
         //public int Id { get; set; }
+        [Required(ErrorMessage = "La calificación es requerida")]
+        [Range(0, 5, ErrorMessage = "La calificación debe estar entre 0 y 5")]
         public int CalificacionUser { get; set; }
+
+        [Required(ErrorMessage = "Los comentarios son requeridos")]
         public string Comentarios { get; set; }
+
         //public DateTime FechaHoraRegistro { get; set; }
+
         //public string UsuarioCalificaId { get; set; }
+
+        [Required(ErrorMessage = "El usuario calificado es requerido")]
         public string UsuarioCalificadoId { get; set; }
     }
 }
