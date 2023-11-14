3e4f574025be1f703cb151a5a49cdcb5e71230d7
Brayan-Garcia-Duarte <brayangar20202@gmail.com>
2023-11-14 01:17:34 -0600

    Ya estan listas pantallas, para hacer pruebas

--- src/APPMovil/src/Api/Api.ts
+++ src/APPMovil/src/Api/Api.ts
@@ -3,7 +3,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 //DEV (REQUIRE NGROK)
-const baseURL = 'https://402c-2806-103e-15-3431-4c29-fc0d-4273-a699.ngrok-free.app/api'
+const baseURL = 'https://bfdd-2806-103e-15-3431-f9dd-c1c0-e0cf-2000.ngrok-free.app/api'
 const API = axios.create({ baseURL });
 
 API.interceptors.request.use(
